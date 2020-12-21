a50ff83466edaf0a3c6097df4ac0bfd98d9e1bb7
DragonOfMercy <dragonofmercy@gmail.com>
2020-12-21 14:37:02 +0100

    Remove useless file

--- build/assets/background.js
+++
@@ -1,9 +0,0 @@
-chrome.commands.onCommand.addListener(function(command) {
-    console.log(command);
-    if (command === 'dragon-open-history') {
-        chrome.tabs.create({
-            active: true,
-            url:  'history.html'
-        }, null);
-    }
-});
